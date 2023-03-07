c78e20c7cbcaf7bcfd1633cd1283e9745918a774
bobcorp <bobcorp99@mail.ru>
2023-03-07 09:58:49 +0500

    ver 1.01 stable

---
+++ src/api/project-work/controllers/project-work.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * project-work controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::project-work.project-work');
---
+++ src/api/project-work/routes/project-work.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * project-work router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::project-work.project-work');
